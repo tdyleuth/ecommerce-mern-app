f52915f66322fc7723730d79b8599ae6d3457fc5
tdyleuth <tony.dyleuth@gmail.com>
2021-01-24 14:11:29 -0800

    Added middleware

--- server/server.js
+++ server/server.js
@@ -2,10 +2,18 @@ require('dotenv').config;
 const express = require('express');
 const connectDB = require('./config/db');
 const productRoutes = require('./routes/productRoutes');
-
-connectDB();
-
 const app = express();
+
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+
+//Middleware
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.json());
 
@@ -14,3 +22,5 @@ app.use('/api/products', productRoutes);
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
+
+connectDB();
